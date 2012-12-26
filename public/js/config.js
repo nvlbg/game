43cb0dfceca7c76d9933e97cb00089490dbb9a6b
Nikolay Lazarov <nvlbg@nvlbg-desktop.(none)>
2012-12-26 15:59:55 +0200

    Cleans up game structure

---
+++ public/js/config.js
@@ -0,0 +1,9 @@
+(function() {
+	window.game = {};
+
+	// configuring system variables
+	me.sys.pauseOnBlur = false;
+	me.sys.useNativeAnimFrame = true;
+	me.sys.interpolation = true;
+	me.sys.fps = 60;
+})();
